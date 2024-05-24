ea694462ba74da6bcae4fa4a89e2fdf9a7060014
mohammedasobur <mohammedasobur0@gmail.com>
2024-05-23 18:55:10 -1200

    two loaders

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Loader from './loader/Loader'
+import './App.css'
+// import Loader2 from './loader2/Loader2'
+
+function App() {
+
+  return (
+    <>
+      <Loader />
+      {/* <Loader2 /> */}
+    </>
+  )
+}
+
+export default App
